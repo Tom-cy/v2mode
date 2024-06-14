ab4b489cdf1e4c5f3832a1ff7e6bc778e34278a5
chenyuan <1980254048@qq.com>
2024-06-14 15:13:06 +0800

    fix:11

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,14 @@
       我是son
       我是son
     </div>
+
+    <div class="ces4">
+      我是son
+      我是son
+      我是son
+      我是son
+      我是son
+    </div>
   </div>
 </template>
 
@@ -20,21 +28,21 @@ export default {
   },
   methods: {
     loadFrame() {
-      const iframe = document.getElementById("iframe");
-      // 设置父页面和iframe页面的document.domain为共同的顶级域
-      document.domain = "chen1995.com";
-      iframe.contentWindow.document.domain = "chen1995.com";
-      // 向iframe中添加CSS
-      const head = iframe.contentWindow.document.head;
-      const cssLink = document.createElement("link");
-      cssLink.href = "http://ddd.chen1995.com/static/css/ifram.css";
-      cssLink.rel = "stylesheet";
-      cssLink.type = "text/css";
+      // const iframe = document.getElementById("iframe");
+      // // 设置父页面和iframe页面的document.domain为共同的顶级域
+      // document.domain = "chen1995.com";
+      // iframe.contentWindow.document.domain = "chen1995.com";
+      // // 向iframe中添加CSS
+      // const head = iframe.contentWindow.document.head;
+      // const cssLink = document.createElement("link");
+      // cssLink.href = "http://ddd.chen1995.com/static/css/ifram.css";
+      // cssLink.rel = "stylesheet";
+      // cssLink.type = "text/css";
 
-      console.log("cssLink", cssLink);
-      console.log("head", head);
+      // console.log("cssLink", cssLink);
+      // console.log("head", head);
 
-      head.appendChild(cssLink);
+      // head.appendChild(cssLink);
     },
   },
 };
